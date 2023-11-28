fd35482130f700df5a61b12c9ebf9b4d4167e385
zolanaj <alexander.zolan@nrel.gov>
2023-11-28 15:19:43 -0700

    add option to flip z axis for plotting comparison

--- code/sp_module.py
+++ code/sp_module.py
@@ -132,13 +132,17 @@ class SolarPilot:
         if name is not None:
             plt.savefig("./../outputs/"+name) 
         
-    def plot_flux_map(self, flux, name = None):
+    def plot_flux_map(self, flux, name = None, flip_z_axis = False):
+
         plot_height = float(self.receiver_data["height"])
         if self.receiver_data["receiver_type"] == 'Flat plate':
             plot_width = float(self.receiver_data["length"])
         if self.receiver_data["receiver_type"] == 'External cylindrical':
             plot_width = 360
-            
+        
+        if flip_z_axis: #use for SolarPILOT imports - this is done automatically in HALOS
+            flux = flux[::-1]
+
         im = plt.imshow(flux, aspect = 'auto', extent = (-plot_width/2, plot_width/2, 0, plot_height))
         plt.colorbar(im)
         plt.ylabel('Receiver vertical position [m]')
